411dc124bc4649c95436ed1a2b98fb4f8fbc7fe7
Seong Kang <seong.h.kang2@gmail.com>
2024-07-25 20:24:39 -0500

    api protection

--- src/Utility/ApiCalls.ts
+++ src/Utility/ApiCalls.ts
@@ -1,19 +1,22 @@
-
 import type { Record } from './Types';
 
 const fetchArtRecords = async (): Promise<Record[]> => {
-    
-    try {
-        const res = await fetch('https://www.rijksmuseum.nl/api/en/collection?key=Ac7mP6Ke&technique=brush&ps=30')
-        if (!res.ok) {
-            throw new Error('Failed to fetch')
-        }
-        const data = await res.json()
-        return data.artObjects
-    } catch (error) {
-        console.error('Failed to fetch records.', error)
-        throw error
-    }
-}
+    const apiKey = process.env.REACT_APP_API_KEY;
+    console.log('API Key:', apiKey);
 
-export  {fetchArtRecords}
+    const url = `https://www.rijksmuseum.nl/api/en/collection?key=${apiKey}&technique=brush&ps=30`;
+
+    try {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error('Failed to fetch');
+        }
+        const data = await res.json();
+        return data.artObjects;
+    } catch (error) {
+        console.error('Failed to fetch records.', error);
+        throw error;
+    }
+};
+
+export { fetchArtRecords };
